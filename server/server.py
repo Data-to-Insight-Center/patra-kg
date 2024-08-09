aa495a557a485483e05041d8f907f896d56ef70a
Sachith Withana <swsachith@gmail.com>
2024-08-09 12:09:35 -0400

    adding swaggerhub to the server.

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 import json
 
 from flask import Flask, request, jsonify
+from flask_restx import Api, Resource, fields
 import os
 from dotenv import load_dotenv
 
@@ -16,67 +17,81 @@ mc_ingester = MCIngester(NEO4J_URI, NEO4J_USERNAME, NEO4J_PWD)
 mc_reconstructor = MCReconstructor(NEO4J_URI, NEO4J_USERNAME, NEO4J_PWD)
 
 app = Flask(__name__)
-
-@app.route('/upload_mc', methods=['POST'])
-def upload_model_card():
-    """
-    Upload model card to the Patra Knowledge Graph.
-    :return: model_card_id
-    """
-    data = request.get_json()
-    base_mc_id = mc_ingester.add_mc(data)
-    return jsonify({"message": "Successfully uploaded the model card", "model_card_id": base_mc_id}), 200
+api = Api(app, version='1.0', title='Patra API',
+          description='API to interact with Patra Knowledge Graph',
+          doc='/swagger')
 
 
-@app.route('/upload_ds', methods=['POST'])
-def upload_datasheet():
-    """
-    Upload datasheet to the Patra Knowledge Graph.
-    """
-    datasheet = request.get_json()
-    mc_ingester.add_datasheet(datasheet)
-    return jsonify({"message": "Successfully uploaded the datasheet"}), 200
+@app.route('/')
+def home():
+    return "Welcome to the Patra Knowledge Base", 200
+
+@api.route('/upload_mc')
+class UploadModelCard(Resource):
+    def post(self):
+        """
+        Upload model card to the Patra Knowledge Graph.
+        Expects a JSON payload.
+        """
+        data = request.get_json()
+        base_mc_id = mc_ingester.add_mc(data)
+        return {"message": "Successfully uploaded the model card", "model_card_id": base_mc_id}, 200
 
 
-@app.route('/search', methods=['GET'])
-def search_kg():
-    """
-    Full text search for model cards using full text search.
-    NOTE: THIS IS NOT PRODUCTION GRADE YET. NEEDS TESTING.
-    """
-    query = request.args.get('q')
-    if not query:
-        return jsonify({"error": "Query (q) is required"}), 400
-
-    results = mc_reconstructor.search_kg(query)
-    return json.dumps(results), 200
+@api.route('/upload_ds')
+class UploadDatasheet(Resource):
+    def post(self):
+        """
+        Upload datasheet to the Patra Knowledge Graph.
+        Expects a JSON payload.
+        """
+        datasheet_data = request.get_json()
+        mc_ingester.add_datasheet(datasheet_data)
+        return {"message": "Successfully uploaded the datasheet"}, 200
 
 
-@app.route('/download_mc', methods=['GET'])
-def download_model_card():
-    """
-    Download a reconstructed model card from the Patra Knowledge Graph.
-    """
-    mc_id = request.args.get('id')
-    if not mc_id:
-        return jsonify({"error": "ID is required"}), 400
+@api.route('/search')
+class SearchKG(Resource):
+    @api.param('q', 'The search query')
+    def get(self):
+        """
+        Full text search for model cards.
+        """
+        query = request.args.get('q')
+        if not query:
+            return {"error": "Query (q) is required"}, 400
 
-    # reconstruct the model card from the Patra Knowledge Graph.
-    model_card = mc_reconstructor.reconstruct(str(mc_id))
-
-    if model_card is None:
-        return jsonify({"error": "Model card could not be found!"}), 400
-
-    return model_card, 200
+        results = mc_reconstructor.search_kg(query)
+        return results, 200
 
 
-@app.route('/list', methods=['GET'])
-def list_models():
-    """
-    Lists all the models in Patra KG.
-    """
-    model_card_dict = mc_reconstructor.get_all_mcs()
-    return json.dumps(model_card_dict, indent=4), 200
+@api.route('/download_mc')
+class DownloadModelCard(Resource):
+    @api.param('id', 'The model card ID')
+    def get(self):
+        """
+        Download a reconstructed model card from the Patra Knowledge Graph.
+        """
+        mc_id = request.args.get('id')
+        if not mc_id:
+            return {"error": "ID is required"}, 400
+
+        model_card = mc_reconstructor.reconstruct(str(mc_id))
+
+        if model_card is None:
+            return {"error": "Model card could not be found!"}, 400
+
+        return model_card, 200
+
+
+@api.route('/list')
+class ListModels(Resource):
+    def get(self):
+        """
+        Lists all the models in Patra KG.
+        """
+        model_card_dict = mc_reconstructor.get_all_mcs()
+        return model_card_dict, 200
 
 
 if __name__ == '__main__':
